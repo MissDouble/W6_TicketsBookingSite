a0ed631ff5027077e29eece1b2af30a14506304b
DoDo <doubleplus37@gmail.com>
2023-08-24 22:52:30 +0800

    更新會員登入及加入swiper失敗

--- assets/swiper.js
+++ assets/swiper.js
@@ -1,9 +1,3 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
-// import './layout/swiper.js';
-
-
-console.log("Hello world!");
 const swiper = new Swiper(".mySwiper", {
     speed: 5000,
     slidesPerView: "auto",
